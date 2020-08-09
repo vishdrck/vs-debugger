c2e35846992e04dafd8541350b0cedb051e2a100
Vishwajith <vish.drck@gmail.com>
2020-08-10 00:36:13 +0530

    Added colors

--- lib/index.ts
+++ lib/index.ts
@@ -1,16 +1,21 @@
-import { CONSOLE_TYPES } from './types/types';
+import { CONSOLE_TYPES, COLORS } from './types/types';
+
 export class VSDebugger {
     /**
      * Enable the debugger.
      * @default false
      */
-    private enable = false;
+    private enabled = false;
 
     // Singleton Data Store
     private static instance: VSDebugger;
 
-    constructor(enable: boolean = false) {
-        this.enable = enable;
+    constructor() {
+
+    }
+
+    public enable(enable: boolean) {
+        this.enabled = enable;
     }
 
     /**
@@ -19,7 +24,7 @@ export class VSDebugger {
      * 
      *   var debugger = new VSDebugger(true).getDebugger();
      */
-    private static getDebugger() {
+    public static getDebugger() {
         if (!this.instance) {
             this.instance = new VSDebugger();
         }
@@ -36,8 +41,8 @@ export class VSDebugger {
      *     var debugger = new VSDebugger(true).getDebugger();
      *     debugger.print(CONSOLE_TYPES.LOG,'Test print function',{name: 'John Doe'});
      */
-    private print(console_type: CONSOLE_TYPES = CONSOLE_TYPES.LOG, msg: string, data: any) {
-        if (this.enable) {
+    public print(console_type: CONSOLE_TYPES = CONSOLE_TYPES.LOG, msg: string, data: any) {
+        if (this.enabled) {
             if (console_type.localeCompare(CONSOLE_TYPES.LOG) === 0) {
                 this.print_log(msg, data);
             } else if (console_type.localeCompare(CONSOLE_TYPES.ERROR) === 0) {
@@ -53,16 +58,29 @@ export class VSDebugger {
     }
 
     private print_log(msg: string, data: any) {
-        console.log(`Log: ${new Date(Date.now()).toLocaleDateString()} * ${msg} => ${data}`);
+        console.group(COLORS.bg.Green, COLORS.fg.Black, '   LOG:   ', COLORS.Reset);
+        console.log(COLORS.fg.Green, `${new Date(Date.now()).toLocaleDateString()} * ${msg} => ${data}`, COLORS.Reset);
+        console.groupEnd();
     }
     private print_warn(msg: string, data: any) {
-        console.warn(`Warning: ${new Date(Date.now()).toLocaleDateString()} * ${msg} => ${data}`);
+        console.group(COLORS.bg.Magenta, COLORS.fg.Yellow, '   WARN:  ', COLORS.Reset);
+        console.warn(COLORS.fg.Magenta, `${new Date(Date.now()).toLocaleDateString()} * ${msg} => ${data}`, COLORS.Reset);
+        console.groupEnd();
     }
     private print_error(msg: string, data: any) {
-        console.log(`Error: ${new Date(Date.now()).toLocaleDateString()} * ${msg} => ${data}`);
+        console.group(COLORS.bg.Red, COLORS.fg.Yellow, '  ERROR:  ', COLORS.Reset);
+        console.log(COLORS.fg.Red, `${new Date(Date.now()).toLocaleDateString()} * ${msg} => ${data}`, COLORS.Reset);
+        console.groupEnd();
     }
     private print_info(msg: string, data: any) {
-        console.log(`Info: ${new Date(Date.now()).toLocaleDateString()} * ${msg} => ${data}`);
+        console.group(COLORS.bg.Blue, COLORS.fg.Yellow, '   INFO:  ', COLORS.Reset);
+        console.log(COLORS.fg.Blue, `${new Date(Date.now()).toLocaleDateString()} * ${msg} => ${data}`, COLORS.Reset);
+        console.groupEnd();
+    }
+
+    public print_obj(msg: any, obj: Object) {
+        console.log(`Log: %c${msg}`, 'color: lightgreen; font-style: italic;')
+        console.table(obj);
     }
 
 };
